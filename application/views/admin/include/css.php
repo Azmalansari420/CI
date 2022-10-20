6d91514099abc7c5d45236fed3f502562fd092b0
Azmalansari420 <114395465+Azmalansari420@users.noreply.github.com>
2022-10-20 16:47:31 +0530

    Add files via upload

---
+++ application/views/admin/include/css.php
@@ -0,0 +1,29 @@
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+
+	<?php 
+	$user = $this->crud->selectDataByMultipleWhere('tbl_admin',array('id'=>$this->session->userdata("id"),));
+	foreach($user as $data)
+		{ ?>
+	<link rel="icon" href="<?php echo base_url(); ?>media/uploads/<?php echo $data->image; ?>" type="image/png" />
+<?php } ?>	
+
+	<!--plugins-->
+	<link href="<?php echo base_url(); ?>media/admin/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
+	<link href="<?php echo base_url(); ?>media/admin/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+	<link href="<?php echo base_url(); ?>media/admin/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+	<link href="<?php echo base_url(); ?>media/admin/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+	
+	<!-- loader-->
+	<!-- <link href="<?php echo base_url(); ?>media/admin/css/pace.min.css" rel="stylesheet" /> -->
+	<script src="<?php echo base_url(); ?>media/admin/js/pace.min.js"></script>
+	<script src="<?php echo base_url(); ?>media/admin/js/tiny.js"></script>
+	
+
+	<!-- Bootstrap CSS -->
+	<link href="<?php echo base_url(); ?>media/admin/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>media/admin/css/app.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>media/admin/css/icons.css" rel="stylesheet">
---
+++ application/views/admin/include/left-sidebar.php
@@ -0,0 +1,88 @@
+<!--sidebar wrapper -->
+		<div class="sidebar-wrapper" data-simplebar="true">
+			<div class="sidebar-header">
+				<!-- <div>
+					<img src="<?php echo base_url(); ?>media/admin/images/avatars/user.png" class="logo-icon" alt="logo icon">
+				</div> -->
+				<div>
+					<h4 class="logo-text">
+						<img src="<?php echo base_url(); ?>media/admin/images/avatars/ad2.png" class="logo-icon" alt="logo icon" style="width: 160px;">
+					</h4>
+				</div>
+				<div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+				</div>
+			</div>
+			<!--navigation-->
+
+			
+			<ul class="metismenu" id="menu">
+
+
+				<li class="menu-label"></li> 
+
+				<li>
+					<a href="<?php echo base_url('admin/dashboard');?>">
+						<div class="parent-icon">
+							<i class='bx bx-home-circle'></i>
+						</div>
+						<div class="menu-title">Dashboard</div>
+					</a>					
+				</li>
+
+
+				<li>
+					<a href="<?php echo base_url('admin_con/site_setting/edit/1');?>">
+						<div class="parent-icon">
+							<i class="lnifadeIn animated bx bx-transfer-alt"></i>
+						</div>
+						<div class="menu-title">Site Setting</div>
+					</a>
+				</li>
+
+				<li>
+					<a href="<?php echo base_url('admin_con/slider/listing');?>">
+						<div class="parent-icon">
+							<i class="lni fadeIn animated bx bx-transfer-alt"></i>
+						</div>
+						<div class="menu-title">Slider</div>
+					</a>
+				</li>
+
+			
+				
+				<li>
+					<a href="<?php echo base_url('admin_con/testimonials/listing');?>">
+						<div class="parent-icon">
+							<i class="lni fadeIn animated bx bx-transfer-alt"></i>
+						</div>
+						<div class="menu-title">Testimonials</div>
+					</a>
+				</li>
+
+				
+
+				<!-- <li class="menu-label">Forms & Tables</li>
+
+				<li>
+					<a class="has-arrow" href="javascript:;">
+						<div class="parent-icon"> <i class="bx fadeIn animated bx bx-transfer-alt"></i>
+						</div>
+						<div class="menu-title">Icons</div>
+					</a>
+					<ul>
+						<li> <a href="icons-line-icons.html"><i class="bx bx-right-arrow-alt"></i>Line Icons</a>
+						</li>
+						<li> <a href="icons-boxicons.html"><i class="bx bx-right-arrow-alt"></i>Boxicons</a>
+						</li>
+						<li> <a href="icons-feather-icons.html"><i class="bx bx-right-arrow-alt"></i>Feather Icons</a>
+						</li>
+					</ul>
+				</li> -->
+
+
+
+			</ul>
+			<!--end navigation-->
+			<!-- Azmal Ansari -->
+		</div>
+		<!--end sidebar wrapper -->
